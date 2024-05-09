0aec0cb73f69e92048b16fd30bbac4a415627672
ypsitau <ypsitau@nifty.com>
2024-05-09 21:56:20 +0900

    implementing avrt

--- examples/Port_InputAnalog/Port_InputAnalog.ino
+++ examples/Port_InputAnalog/Port_InputAnalog.ino
@@ -4,6 +4,7 @@ namespace av = avrt;
 
 AVRT_IMPLEMENT_Serial0(serial)
 
+av::Port<av::D2> portD2;
 av::Port<av::A0> portA0;
 av::Port<av::A1> portA1;
 av::Port<av::A2> portA2;
@@ -16,13 +17,24 @@ void setup()
 {
 	serial.Open(av::Serial::BaudRate57600);
 	av::InitPort<>();
-	av::InitADC<>();
+	av::InitADC<
+		0b0000,		// MUX: Analog Channel Selection Bits = ADC0
+		0b000		// ADPS: ADC Prescaler Select Bits = 1/128
+	>();
+	portD2.SetMode<av::Out>();
 	serial.Println(F("Analog Print"));
+	for (;;) {
+		portD2.ToggleDigital();
+		//portD2.OutputDigital<av::High>();
+		//portD2.OutputDigital<av::High>();
+		//portD2.OutputDigital<av::High>();
+		portA0.InputAnalog();
+	}
 }
 
 void loop()
 {
 	serial.Printf(F("A0:%-4d A1:%-4d A2:%-4d A3:%-4d A4:%-4d A5:%-4d A6:%-4d\n"),
-		portA0.InputAnalog(), portA1.InputAnalog(), portA2.InputAnalog(),
+	portA0.InputAnalog(), portA1.InputAnalog(), portA2.InputAnalog(),
 		portA3.InputAnalog(), portA4.InputAnalog(), portA5.InputAnalog(), portA6.InputAnalog());
 }
