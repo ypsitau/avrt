5b7973185c4350c95e0cad328255a017ed171784
ypsitau <ypsitau@nifty.com>
2024-05-13 20:40:19 +0900

    implementina avrt

--- src/ADConv.h
+++ src/ADConv.h
@@ -19,11 +19,12 @@ template <
 	uint8_t dataREFS	= 0b01		// REFS: Reference Selction Bits = AVcc with external capacitor at AREF pin 
 > class ADConv {
 public:
+	constexpr static uint8_t dataADSC = 0b0;	// ADSC: ADC Start Conversion = false
+	constexpr static uint8_t dataADIF = 0b1;	// ADIF: ADC Interrupt Flag .. set one to clear
+	constexpr static uint8_t dataADEN = 0b1;	// ADEN: ADC Enable = true
+public:
 	void Init() const {
 		constexpr uint8_t dataADLAR = 0b0;	// ADLAR: ADC Left Adjust Result = false
-		constexpr uint8_t dataADSC	= 0b0;	// ADSC: ADC Start Conversion = false
-		constexpr uint8_t dataADIF	= 0b1;	// ADIF: ADC Interrupt Flag .. set one to clear
-		constexpr uint8_t dataADEN	= 0b1;	// ADEN: ADC Enable = true
 		ADMUX = (dataREFS << REFS0) | (dataADLAR << ADLAR) | (dataMUX << MUX0);
 		ADCSRA = (dataADEN << ADEN) | (dataADSC << ADSC) | (dataADATE << ADATE) |
 			(dataADIF << ADIF) | (dataADIE << ADIE) | (dataADPS << ADPS0);
@@ -31,9 +32,6 @@ public:
 	}
 	void InitAs8bit() const {
 		constexpr uint8_t dataADLAR = 0b1;	// ADLAR: ADC Left Adjust Result = true
-		constexpr uint8_t dataADSC	= 0b0;	// ADSC: ADC Start Conversion = false
-		constexpr uint8_t dataADIF	= 0b1;	// ADIF: ADC Interrupt Flag .. set one to clear
-		constexpr uint8_t dataADEN	= 0b1;	// ADEN: ADC Enable = true
 		ADMUX = (dataREFS << REFS0) | (dataADLAR << ADLAR) | (dataMUX << MUX0);
 		ADCSRA = (dataADEN << ADEN) | (dataADSC << ADSC) | (dataADATE << ADATE) |
 			(dataADIF << ADIF) | (dataADIE << ADIE) | (dataADPS << ADPS0);
