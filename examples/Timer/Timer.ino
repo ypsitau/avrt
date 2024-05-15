a952603fb0deaacabce20ef249796ff0b431be13
ypsitau <ypsitau@nifty.com>
2024-05-15 16:25:42 +0900

    implementing Timer and ADConv

--- examples/Timer/Timer.ino
+++ examples/Timer/Timer.ino
@@ -7,15 +7,9 @@ AVRT_IMPLEMENT_Serial0(serial)
 
 av::Port<av::D2> portD2;
 
-av::Timer0<
-	0b1		// TOIE0: Timer/Counter0 Overflow Interrupt Enable (TIMER0_OVF) = enable
-> timer0;
+av::Timer0<true> timer0;	// enable interrupt: TIMER0_OVF 
 
 av::Timer1<
-	//0b011,	// CS1: Clock Select = clk/64
-	0b001,	// CS1: Clock Select = clk/1
-	0b001,	// WGM1: Waveform Generation Mode = PWM, Phase Correct, 8-bit
-	//0b101,	// WGM1: Waveform Generation Mode = Fast PWM, 8-bit
 	0b1		// TOIE1: Timer/Counter1 Overflow Interrupt Enable (TIMER1_OVF) = enable
 > timer1;
 
@@ -42,7 +36,7 @@ void setup()
 	serial.Open(serial.BaudRate57600);
 	portD2.SetMode<av::Out>();
 	timer0.Start(timer0.Clock::Div64, timer0.Waveform::FastPWM_UptoFF);
-	timer1.Start();
+	timer1.Start(timer1.Clock::Div64, timer1.Waveform::FastPWM_Upto00FF);
 	timer2.Start();
 }
 
