88d3f505a15c8fc19a798ea76d3856e776d1afc7
ypsitau <ypsitau@nifty.com>
2024-05-23 23:01:50 +0900

    implementing TwoWire

--- examples/test-TwoWire/test-TwoWire.ino
+++ examples/test-TwoWire/test-TwoWire.ino
@@ -10,6 +10,21 @@ public:
 	void SendGeneric(uint8_t code, uint8_t rsBit);
 	void SendCommand(uint8_t code) { SendGeneric(code, 0b0); }
 public:
+	void ClearDisplay() { SendCommand(0x01); }
+	void ReturnHome() { SendCommand(0x02); }
+	void Clear() { ClearDisplay(); ReturnHome(); }
+	void EntryModeSet(bool incFlag, bool shiftFlag) {
+		SendCommand(0x04 | (static_cast<uint8_t>(incFlag) << 1) | (static_cast<uint8_t>(shiftFlag) << 0));
+	}
+	void DisplayOnOffControl(bool displayFlag, bool cursorFlag, bool blinkFlag) {
+		SendCommand(0x08 | (static_cast<uint8_t>(displayFlag) << 2) | (static_cast<uint8_t>(cursorFlag) << 1) | (static_cast<uint8_t>(blinkFlag) << 0));
+	}
+	void CursorOrDisplayShift() { SendCommand(0x10); }
+	void FunctionSet() { SendCommand(0x20); }
+	void SetCGRAMAddress() { SendCommand(0x40); }
+	void SetDDRAMAddress() { SendCommand(0x80); }
+	//void ReadBusyFlagAndAddressCounter() { SendCommand(0x01); }
+public:
 	virtual void SendData(uint8_t data) override { SendGeneric(data, 0b1); }
 	virtual uint8_t RecvData() override { /* nothing */ }
 };
@@ -34,26 +49,32 @@ void LCD1602::SendGeneric(uint8_t code, uint8_t rsBit)
 
 namespace av = avrt;
 
-AVRT_IMPLEMENT_Timer1(timer1)
-AVRT_IMPLEMENT_TwoWire(twi, timer1)
+AVRT_IMPLEMENT_Timer1(timer)
+AVRT_IMPLEMENT_TwoWire(twi, timer)
 AVRT_IMPLEMENT_Serial0_NoRecv(serial)
+
+av::LCD1602 lcd(twi);
 
 void setup()
 {
 	twi.Open();
-	timer1.Start(timer1.Clock::Div64, timer1.Waveform::PhaseCorrectPWM_Upto01FF, timer1.EnableInt_TIMER1_OVF);
+	timer.Start(timer.Clock::Div64, timer.Waveform::PhaseCorrectPWM_Upto01FF, timer.EnableInt_TIMER1_OVF);
 	serial.Open(serial.Speed::Bps57600);
 	serial.Printf("test-TwoWire\n");
-	av::LCD1602 lcd(twi);
-	lcd.SendCommand(0x01);
-	lcd.SendCommand(0x02);
-	lcd.SendCommand(0x06);
-	lcd.SendCommand(0x0c);
-	lcd.SendCommand(0x2c);
-	const char buff[] = "Hello World2";
-	for (const char* p = buff; *p; p++) lcd.SendData(*p);
+	lcd.ClearDisplay();
+	lcd.ReturnHome();
+	lcd.EntryModeSet(true, false);
+	lcd.DisplayOnOffControl(true, false, false);
+	//lcd.SendCommand(0x2c);
 }
 
 void loop()
 {
+	lcd.Clear();
+	lcd.Printf(F("ABCDEFG"));
+	lcd.Printf(F("ABCDEFG"));
+	timer.DelayMSec(500);
+	lcd.Clear();
+	lcd.Printf(F("Hello"));
+	timer.DelayMSec(500);
 }
