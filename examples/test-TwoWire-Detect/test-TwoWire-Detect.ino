c50d1697fdcd175ab114d756c9532661fe28f739
ypsitau <ypsitau@nifty.com>
2024-05-30 21:06:38 +0900

    implementing TwoWIre

---
+++ examples/test-TwoWire-Detect/test-TwoWire-Detect.ino
@@ -0,0 +1,20 @@
+#include <avrt.h>
+#include <TwoWire.h>
+
+namespace av = avrt;
+
+AVRT_IMPLEMENT_Timer1(timer)
+AVRT_IMPLEMENT_TwoWire(twi, timer)
+AVRT_IMPLEMENT_Serial0_NoRecv(serial)
+
+void setup()
+{
+	timer.Start(timer.Clock::Div64, timer.Waveform::PhaseCorrectPWM_Upto01FF, timer.EnableInt_TIMER1_OVF);
+	serial.Open(serial.Speed::Bps57600);
+	serial.Printf(F("#### test-TwoWire-Detect ####\n"));
+	twi.Open();
+	twi.Detect(serial);
+}
+
+void loop() {
+}
